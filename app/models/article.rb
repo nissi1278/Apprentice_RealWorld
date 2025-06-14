c736b95f13400cceaa14c053cc384ed08b995248
nissi <nissi1270561@gmail.com>
2025-06-14 06:38:38 +0000

    fix: 関連付けのモデル名が間違っていたため修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
     has_many :article_tags, dependent: :destroy
     has_many :tags, through: :article_tags
-    belongs_to :users
+    belongs_to :user
 
     validates :title, presence: true, length: { minimum: 5 }, uniqueness: true
     validates :about, :description, presence: true
