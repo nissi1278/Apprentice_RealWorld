96df3f0d16c7b5ba7ae101eff59bb0f1c3cc7969
にっしー <nissi1270561@gmail.com>
2025-06-24 13:56:06 +0000

    ref: emailのバリデーションにformメゾットを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   has_many :articles, dependent: :destroy
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 5 }
 end
